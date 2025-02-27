8faf9193a20dee04fcde2dca55cd5f4d32a0b807
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-02-27 18:12:42 +0000

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@piggycoin/sdk'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 
-import { injected, portis, walletconnect, walletlink } from '../connectors'
+import { injected, walletconnect, walletlink } from '../connectors'
 
 export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
 
